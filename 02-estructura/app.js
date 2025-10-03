fc6daabb44b988d2fe18ada003855d7a97ef22be
cnywu0 <nicolaslol22@gmail.com>
2025-10-03 11:47:43 +0200

    Setting Up: 02-estructura

---
+++ 02-estructura/app.js
@@ -0,0 +1,18 @@
+import express from 'express';
+
+const PORT = 80;
+
+const app = express();
+
+// Modo de uso de JSON
+app.use(express.json());
+
+// Endpoint raiz
+app.get('/', (req, res) => {
+    res.send('Hola Mundo Este es mi primor servidor con express');
+});
+
+// Escuchar en el puerto 80
+app.listen(PORT, () => {
+    console.log('Servidor escuchando en el puerto: ' + PORT);
+})
